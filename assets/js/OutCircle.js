286b0d2c0a8d51097a20142160acd8391f74f87c
Greenleaf06 <dany.graells@hetic.net>
2014-06-13 07:58:47 +0200

    MAJ final Outcircle en dur

--- assets/js/OutCircle.js
+++ assets/js/OutCircle.js
@@ -1,47 +1,43 @@
-function createOutCircle(genocides){
-    
-    var data = [genocides[0].nombreMorts, popAvant];
-    var r = 150;
+var data = [10, 50, 80];
+var r = 300;
 
-    var color = d3.scale.ordinal()
-        .range(['rgb(0,120,50)', 'rgb(0,100,50)', 'rgb(0,80,50)']);
+var color = d3.scale.ordinal()
+    .range(['rgb(0,120,50)', 'rgb(0,100,50)', 'rgb(0,80,50)']);
 
-    var canvas = d3.select('#canvas').append('svg')
-        .attr('width', 500)
-        .attr('height', 500);
+var canvas = d3.select('#canvas').append('svg')
+    .attr('width', 1500)
+    .attr('height', 1500);
 
-    var group = canvas.append('g')
-        .attr('transform', 'translate(300, 300)');
+var group = canvas.append('g')
+    .attr('transform', 'translate(300, 300)');
 
-    var arc = d3.svg.arc()
-        .innerRadius(100)
-        .outerRadius(r);
+var arc = d3.svg.arc()
+    .innerRadius(230)
+    .outerRadius(r-20);
 
-    var pie = d3.layout.pie()
-        .value(function (d) {
-        return d;
-    });
+var pie = d3.layout.pie()
+    .value(function (d) {
+    return d;
+});
 
-    var arcs = group.selectAll('.arc')
-        .data(pie(data))
-        .enter()
-        .append('g')
-        .attr('class', 'arc');
+var arcs = group.selectAll('.arc')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .attr('class', 'arc');
 
-    arcs.append('path')
-        .attr('d', arc)
-        .attr('fill', function (d) {
-        return color(d.data);
-    });
+arcs.append('path')
+    .attr('d', arc)
+    .attr('fill', function (d) {
+    return color(d.data);
+});
 
-    arcs.append('text')
-        .attr('transform', function (d) {
-        return 'translate(' + arc.centroid(d) + ')';
-    })
-        .attr('text-anchor', 'middle')
-        .attr('font-size', '1em')
-        .text(function (d) {
-        return (d.data);
-    });
-
-};
+arcs.append('text')
+    .attr('transform', function (d) {
+    return 'translate(' + arc.centroid(d) + ')';
+})
+    .attr('text-anchor', 'middle')
+    .attr('font-size', '1.5em')
+    .text(function (d) {
+    return d.data;
+});
